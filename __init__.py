79e280f821682edc6173ae21dd09d1404316af1f
bikiam <157801122+bikiam@users.noreply.github.com>
2025-03-26 20:33:38 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .nodes.tutorial_nodes import * 
+
+NODE_CLASS_MAPPINGS = {
+    "Print Hello World": PrintHelloWorld,
+    }
+    
+print("\033[34mComfyUI Tutorial Nodes: \033[92mLoaded\033[0m")  
